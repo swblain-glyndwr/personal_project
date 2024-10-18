6776a7b855749f76281476a608710d606d9cbbcd
Tom Corke <tom_corke@next.co.uk>
2024-10-18 17:23:18 +0100

    Corrected AdFormat to TemplateName

--- next_ads/sql/create_table_nextads_control_sheet_latest.sql
+++ next_ads/sql/create_table_nextads_control_sheet_latest.sql
@@ -10,7 +10,7 @@ create table marketingdata_prod.ds_sandbox.next_uk_nextads_control_sheet_latest 
   Location string,
   Segment string,
   AdDriver string,
-  AdFormat string,
+  TemplateName string,
   StartDate date,
   EndDate date,
   Models string,
