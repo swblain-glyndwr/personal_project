6f6c545437459bbf8a1d65f086bfd5d8bf62c8e2
Tom Corke <tom_corke@next.co.uk>
2024-10-30 16:07:26 +0000

    Added breaker to while loop to cover case that recursion upward through directories does not find .git directory

--- next_ads/utils/gitcontext.py
+++ next_ads/utils/gitcontext.py
@@ -7,7 +7,10 @@ def find_git_head() -> Path:
     git_dir = Path(os.getcwd()).parent
 
     while not (git_dir/".git").exists():
+        git_dir_prev = git_dir
         git_dir = git_dir.parent
+        if git_dir == git_dir_prev:
+            raise Exception("No .git directory not found")
 
     return (git_dir/".git"/"HEAD")
 
