c670465a7c9906efb32533b4b5052c958ffb210b
Tom Corke <tom_corke@next.co.uk>
2024-11-01 15:44:39 +0000

    Corrected boolean datatype

--- next_ads/sql/create_table_fixed_cells.sql
+++ next_ads/sql/create_table_fixed_cells.sql
@@ -1,6 +1,6 @@
 create table marketingdata_prod.ds_sandbox.next_uk_nextads_fixed_cells (
     AccountNumber string not null,
-    FallowControl bool not null,
+    FallowControl boolean not null,
     AlgoDivision string not null,
     MacroLocation string not null,
     MacroLocationCell string not null,
