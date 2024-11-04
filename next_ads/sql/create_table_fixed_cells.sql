166f5a740563d08de7fbed81742f97c3ce337835
Tom Corke <tom_corke@next.co.uk>
2024-11-04 16:05:41 +0000

    Changed fixed cell table definition from long to wide

--- next_ads/sql/create_table_fixed_cells.sql
+++ next_ads/sql/create_table_fixed_cells.sql
@@ -1,16 +1,19 @@
 create table marketingdata_prod.{schema}.next_uk_nextads_fixed_cells (
     AccountNumber string not null,
     FallowControl boolean not null,
-    AlgoDivision string not null,
-    MacroLocation string not null,
-    MacroLocationCell string not null,
+    HN string not null,
+    SB string not null,
+    OC string not null,
+    LP string not null,
+    AdHocAB1 string,
+    AdHocAB2 string,
+    AdHocAB3 string,
+    AdHocAB4 string,
+    ChampionChallenger string,
+    AlgoDivision string,
     rundate date not null,
   constraint pk_next_uk_nextads_fixed_cells primary key (
-    AccountNumber,
-    FallowControl,
-    AlgoDivision,
-    MacroLocation,
-    MacroLocationCell
+    AccountNumber
     )
 )
 partitioned by (AlgoDivision)
