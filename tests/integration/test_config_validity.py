5310c33335e546d8484190219234774e36988947
Corke, Tom <Tom_Corke@next.co.uk>
2025-07-02 10:11:04 +0000

    Merged PR 152138: Config change: Removed Seasons-Premium audience assignments
    - Removed Seasons/Premium audience and assignments from the config
    - Also updated `test_config_validity.py` test to account for case where there are no Audiences specified in the config
    
    Related work items: #4496829

--- tests/integration/test_config_validity.py
+++ tests/integration/test_config_validity.py
@@ -13,9 +13,11 @@ def test_audiences_required(client):
     with open(f'config/{client}.json') as f:
         cfg = json.load(f)
 
-    audiences_required = [x[0] for x in cfg['transient_cells']['Audiences']]
-    read_tables = list(cfg['tables']['read'].keys())
+    if 'Audiences' in cfg['transient_cells']:
+        audiences_required = [
+            x[0] for x in cfg['transient_cells']['Audiences']]
+        read_tables = list(cfg['tables']['read'].keys())
 
-    for audience in audiences_required:
-        msg = f'Audience key not in read tables: {audience}'
-        assert audience in read_tables, msg
+        for audience in audiences_required:
+            msg = f'Audience key not in read tables: {audience}'
+            assert audience in read_tables, msg
