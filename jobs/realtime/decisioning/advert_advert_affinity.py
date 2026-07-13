d5efed4ad25c76a45c3a04a2a5c2e0071d73aba4
Claire Wilson-Barnes <claire_wilsonbarnes@next.co.uk>
2026-07-13 18:45:25 +0100

    resolve workspace root path

--- jobs/realtime/decisioning/advert_advert_affinity.py
+++ jobs/realtime/decisioning/advert_advert_affinity.py
@@ -1,20 +1,15 @@
 import sys
 from pathlib import Path
-
+import sys
+from pathlib import Path
 try:
     PROJECT_ROOT = Path(__file__).resolve().parents[3]
 except NameError:
     # __file__ is not defined when running as a Databricks notebook
-    notebook_path = (
-        dbutils.notebook.entry_point.getDbutils()
-        .notebook()
-        .getContext()
-        .notebookPath()
-        .get()
-    )  # type: ignore # noqa
-    if not notebook_path.startswith("/Workspace"):
-        notebook_path = "/Workspace" + notebook_path
-    PROJECT_ROOT = Path(notebook_path).parents[2]
+    notebook_path = dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get() # type: ignore # noqa
+    if not notebook_path.startswith('/Workspace'):
+        notebook_path = '/Workspace' + notebook_path
+    PROJECT_ROOT = Path(notebook_path).parents[3]
 finally:
     print(f"Project root resolved to: {PROJECT_ROOT}")
     sys.path.insert(0, str(PROJECT_ROOT))
