15ce764668b79994a944f5298d1b480d151b3bb4
Claire Wilson-Barnes <claire_wilsonbarnes@next.co.uk>
2026-07-13 16:33:06 +0100

    Functionality outline for RT decisioning reranking model

---
+++ src/next_ads/realtime/decisioning/real_time_decisioning_rules_based_model.py
@@ -0,0 +1,19 @@
+
+
+def realtime_reranking_model(input_features: dict)-> dict: 
+
+    
+    #1 Filter batch adverts to current customer
+
+    #2 Filter item table to provided features 
+
+    #3 Cross item table with weighting factors 
+
+    #4 Cross weighting factors with batch adverts 
+
+    #5 Rerank weighting factors 
+
+    #6 Update items 
+
+    #7 Return items
+
