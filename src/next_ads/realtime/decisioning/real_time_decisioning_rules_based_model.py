3a6ae8dcaaab94c63247f7084b014507c35ca4a5
Claire Wilson-Barnes <claire_wilsonbarnes@next.co.uk>
2026-07-28 13:04:24 +0100

    WIP version of rules based model

--- src/next_ads/realtime/decisioning/real_time_decisioning_rules_based_model.py
+++ src/next_ads/realtime/decisioning/real_time_decisioning_rules_based_model.py
@@ -1,36 +1,124 @@
 import mlflow.pyfunc
 from concurrent.futures import ThreadPoolExecutor
 
+import hashlib
+import json
 import logging
 import psycopg2
+import numpy as np
 import pandas as pd
 
 logger = logging.getLogger("realtime_reranking_model")
 
 
 class RealtimeKnownRerankingModel(mlflow.pyfunc.PythonModel):
-    def __init__(self):
-        self.conn = None
-        self.min_number_of_ads = 10
+    def __init__(
+        self,
+        pagetype_filter: list = ["ProductListingPage", "ShoppingBag"],
+        min_number_of_ads: int = 10,
+        ad_fatigue_active_locations: list = [],
+        ad_fatigue_threshold: int = 2,
+        fragment_record_limit: int = 20,
+        trigger_record_limit: int = 5,
+    ):
 
-    def load_context(self):
-        """Initialise the context at the point the model is loaded"""
-        # TODO: Resolve this so is getting oauth token
-        # TODO: if not connecting has retries for reconnecting
-        pass
+        self.conn = None
+        self.pagetype_filter: list = pagetype_filter
+        self.min_number_of_ads: int = min_number_of_ads
+        self.item_feature_columns: list = [
+            "brand",
+            "next_category",
+            "department",
+            "prem_level_brand",
+        ]
+
+        # Payload settings
+        self.ad_fatigue_threshold: int = ad_fatigue_threshold
+        self.ad_fatigue_active_locations: list = ad_fatigue_active_locations
+        self.trigger_record_limit: int = trigger_record_limit
+        self.fragments_record_limit: int = fragment_record_limit
+
+        # TODO: add Audience column in here!
+        self.customer_cells_columns: list = [
+            "AccountNumber",
+            "roamingprofileid",
+            "FallowControl",
+            "ShoppingBagTest1",
+            "OrderCompleteTest1",
+            "LandingPageTest1",
+            "AdHocABTest1",
+            "AdHocABTest2",
+            "AdHocABTest3",
+            "AdHocABTest4",
+            "AdHocABTest5",
+            "AdHocABTest6",
+            "AdHocABTest7",
+            "AdHocABTest8",
+            "AdHocABTest9",
+            "ChampionChallenger",
+            "PageTypeIsolation",
+            "specialaccountindicator",
+            "AlgoDivision",
+            # "Audience",
+            "IsPremium",
+        ]
+        # Settings for experiments
+        # Default set to False so no impact to customer experience if there were to be an issue
+        self.control: bool = False
+        self.control_value: str = "NoAds"
+        self.experiment_settings: dict = {
+            "experiments": {
+                "NextAds": "FallowControl",
+                "PageIsolation": "PageTypeIsolation",
+                "NextGenAds": "AdHocABTest1",
+            },
+            "audience_experiments": {
+                "enabled": True,
+                "split_col": "Audience",
+                "sample": ["Best"],
+                "split": None,
+            },
+        }
+        self.experiment_details = {
+            "NextAds": [
+                ("FallowControl", "NoAds", "CT"),
+                ("ShoppingBagTest1", "Basic", "BA"),
+                ("ShoppingBagTest1", "Best", "BE"),
+            ],
+            "PageIsolation": [
+                ("split_col", "AllPages", "AP"),
+                ("split_col", "PLP_Only", "PL"),
+                ("split_col", "SB_Only", "SB"),
+                ("split_col", "HP_Only", "HP"),
+                ("split_col", "OC_Only", "OC"),
+            ],
+            # Default behavior for any unlisted col_name (A/B testing)
+            "DEFAULT": [
+                ("split_col", "A", "A"),
+                ("split_col", "B", "B"),
+            ],
+        }
+
+    # def load_context(self, context):
+    #     """Initialise the context at the point the model is loaded"""
+
+    #     #TODO: Resolve this so if not connecting has retries for reconnecting
+    #     self.generate_lakebase_connection()
+    #     pass
 
     def generate_lakebase_connection(self):
-        #     #TODO UPDATE HOW THIS WILL WORK FOR A SERVED MODEL
+        #     #TODO FIX HOW THIS WILL WORK FOR A SERVED MODEL -may need to call to get an oauth token
         conn = psycopg2.connect(
-            host=config.ONLINE_RT_LAKEBASE_HOST,
-            dbname=config.ONLINE_RT_FEATURE_STORE_DB_NAME,
-            user=config.ONLINE_RT_FEATURE_STORE_USER,
-            password=config.ONLINE_RT_FEATURE_STORE_PASSWORD,
+            host=ONLINE_RT_LAKEBASE_HOST,
+            dbname=ONLINE_RT_FEATURE_STORE_DB_NAME,
+            user=ONLINE_RT_FEATURE_STORE_USER,
+            password=ONLINE_RT_FEATURE_STORE_PASSWORD,
             sslmode="require",
         )
         self.conn = conn
 
-    def run_query(self, qry):
+    def run_query(self, qry: str) -> pd.DataFrame:
+
         results = pd.DataFrame()
         try:
             if not self.conn:
@@ -45,16 +133,13 @@ class RealtimeKnownRerankingModel(mlflow.pyfunc.PythonModel):
             logger.error(f"Exception in fetching data: {e}")
         return results
 
-    def predict(self, model_input):
-
-        output = {}
-        PageTypeFilters = ["ProductListingPage", "ShoppingBag"]
+    def predict(self, model_input: dict) -> dict:
 
         with ThreadPoolExecutor(max_workers=2) as executor:
             ads = executor.submit(
                 self.realtime_reranking_advert_data_prep,
                 model_input.get("rpid", ""),
-                PageTypeFilters,
+                self.pagetype_filter,
             )
             items = executor.submit(
                 self.realtime_reranking_item_data_prep,
@@ -65,22 +150,22 @@ class RealtimeKnownRerankingModel(mlflow.pyfunc.PythonModel):
         item_df = items.result()
 
         if ad_df.empty | item_df.empty:
-            logger.error("No customer ads data or items data found")
-            return output
+            logger.error("No data identified for items or rpid")
+            return {}
 
         cross_data = self.realtime_reranking_item_cross(ad_df, item_df)
         if cross_data.empty:
-            logger.error("No results from crossed data")
-            return output
+            logger.error("No cross data")
+            return {}
 
         best_ad_df = cross_data[cross_data["AdjustedRanking"] == 1]
         results = self.incrementality(best_ad_df)
 
         if results.empty:
-            logger.error("No incrementality results generated")
-            return output
-        # TODO imporove this
-        # final formatting
+            logger.error("No incrementality results found")
+            # TODO: decide on action here
+
+        # TODO: Finish/Improve the final formatting
         number_records = results.groupby("PageType", as_index=False).agg(
             {"UniqueAdID": "count"}
         )
@@ -90,15 +175,23 @@ class RealtimeKnownRerankingModel(mlflow.pyfunc.PythonModel):
 
         if not missing_records.empty:
             logger.info("Insufficient records for PageTypes")
-            # TODO: take results for each PageType if &  combine with records from cross data. (no dups)
+            # TODO take results for each PageType if &  combine with records from cross data. (no dups)
 
-        output = self.upload_format(results)
+        customer_cells = ad_df[self.customer_cells_columns].drop_duplicates()
 
-        self.write_to_bloomreach(output)
+        payload = self.build_payload_structure(customer_cells, results)
 
-        return output
+        if not payload:
+            logger.error("No payload generated")
+            return {}
 
-    def realtime_reranking_advert_data_prep(self, input_rpid, PageTypeFilters):
+        # TODO write to API
+
+        return payload
+
+    def realtime_reranking_advert_data_prep(
+        self, input_rpid: int, page_type_filter: list
+    ) -> pd.DataFrame:
 
         customer_ads = pd.DataFrame()
 
@@ -120,7 +213,7 @@ class RealtimeKnownRerankingModel(mlflow.pyfunc.PythonModel):
         preranked_ads = self.run_query(ads_qry)
         preranked_ads_df = pd.DataFrame(preranked_ads)
         customer_ads = preranked_ads_df[
-            preranked_ads_df["PageType"].isin(PageTypeFilters)
+            preranked_ads_df["PageType"].isin(page_type_filter)
         ]
 
         if customer_ads.empty:
@@ -128,7 +221,9 @@ class RealtimeKnownRerankingModel(mlflow.pyfunc.PythonModel):
 
         return customer_ads
 
-    def realtime_reranking_item_data_prep(self, input_features: dict):
+    def realtime_reranking_item_data_prep(
+        self, input_features: dict
+    ) -> pd.DataFrame:
 
         items_data = pd.DataFrame()
         flattened_data = [{**value} for value in input_features.values()]
@@ -165,18 +260,23 @@ class RealtimeKnownRerankingModel(mlflow.pyfunc.PythonModel):
 
         return items_data
 
-    # 3 Cross item table with weighting factors
+    def realtime_reranking_item_cross(
+        self, customer_ads: pd.DataFrame, item_df: pd.DataFrame
+    ) -> pd.DataFrame:
 
-    def realtime_reranking_item_cross(self, customer_ads, item_df):
-
-        # 4 Cross weighting factors with batch adverts
-        cols = ["brand", "next_category", "department", "prem_level_brand"]
+        customer_ads["adjusted_weighting_final"] = 0
         customer_ads_ = customer_ads[
-            ["UniqueAdID", "PageType", "Score", "TriggerScore"]
+            [
+                "roamingprofileid",
+                "UniqueAdID",
+                "PageType",
+                "Score",
+                "TriggerScore",
+                "adjusted_weighting_final",
+            ]
         ]
-        customer_ads_["adjusted_weighting_final"] = 0
 
-        for col in cols:
+        for col in self.item_feature_columns:
             cross_df = customer_ads.merge(
                 item_df[item_df[f"weighting_{col}"] != 0],
                 on=col,
@@ -235,6 +335,7 @@ class RealtimeKnownRerankingModel(mlflow.pyfunc.PythonModel):
 
         return customer_ads_[
             [
+                "roamingprofileid",
                 "UniqueAdID",
                 "PageType",
                 "Score",
@@ -246,14 +347,15 @@ class RealtimeKnownRerankingModel(mlflow.pyfunc.PythonModel):
             ]
         ]
 
-    def incrementality(self, best_ad_df):
+    def incrementality(self, best_ad_df: pd.DataFrame) -> pd.DataFrame:
 
         cust_affinity = pd.DataFrame()
+
         # TODO change to variable
         table = "marketingdata_dev.claire_wilsonbarnes.next_uk_nextads_advert_advert_association_online"
 
-        # 2 Filter item table to provided best adverts for each location
         best_ads = "', '".join(best_ad_df["UniqueAdID"].unique())
+
         ad_affinity_qry = f"""SELECT
                             *
                             FROM {table}
@@ -277,6 +379,7 @@ class RealtimeKnownRerankingModel(mlflow.pyfunc.PythonModel):
         ).astype(bool)
         cust_affinity = merged[is_page_type_true][
             [
+                "roamingprofileid",
                 "PageType",
                 "UniqueAdID",
                 "AtbUniqueAdID",
@@ -287,11 +390,162 @@ class RealtimeKnownRerankingModel(mlflow.pyfunc.PythonModel):
 
         return cust_affinity
 
-    def upload_format(self, results_df):
-        # TODO: set up format
-        # format the items for the upload structure
-        pass
+    ## Functions for bulding the payload:
+
+    def determine_control(self, df: pd.DataFrame):
+        if df["FallowControl"] == self.control_value:
+            self.control = True
+
+    def build_triggers(self, df: pd.DataFrame):
+        # Get top scores for ads for the customer
+        triggers = []
+        ##TODO: SWITCH TO CMSPAGEID RATHER THAN ADID!
+        combined = (
+            df.groupby(["roamingprofileid", "AtbUniqueAdID"], as_index=False)
+            .max("lift_adjusted")
+            .sort_values("lift_adjusted", ascending=False)
+            .rename(columns={"lift_adjusted": "t", "AtbUniqueAdID": "id"})
+        )
+        triggers = combined[: self.trigger_record_limit][["t", "id"]].to_dict(
+            "records"
+        )
+        return triggers
+
+    def build_adshash(self, dict_data: dict):
+        # Build the ad hash for the experiment
+        try:
+            string_data = json.dumps(dict_data, sort_keys=True)
+            return hashlib.sha256(string_data.encode("utf-8")).hexdigest()
+        except Exception as e:
+            logger.error(f"Error building ad hash {e}")
+            return ""
+
+    def build_fragments(self, df):
+        # Get top scored ads for the locations
+        fragments = []
+        pages = df["PageType"].unique().tolist()
+        for page in pages:
+            # TODO SWAP AtbUniqueAdID to CMSPAGEID
+            fragment_ids = (
+                df[df["PageType"] == page]
+                .sort_values("lift_adjusted", ascending=False)[
+                    : self.fragments_record_limit
+                ]["AtbUniqueAdID"]
+                .tolist()
+            )
+            fragments.append(
+                {
+                    "pageTypes": [page],
+                    "enableAdFatigueRotation": page
+                    in self.ad_fatigue_active_locations,
+                    "fragmentIds": fragment_ids,
+                }
+            )
+        return fragments
+
+    def get_experiment_id(
+        self, df: pd.DataFrame, col_name: str, split_col_name="split_col"
+    ):
+
+        rules = self.experiment_details.get(
+            col_name, self.experiment_details["DEFAULT"]
+        )
+        conditions = []
+        choices = []
+        for target_col, val, suffix in rules:
+            # Resolve 'split_col' dynamically
+            actual_col = (
+                split_col_name if target_col == "split_col" else target_col
+            )
+            # Build boolean mask condition
+            conditions.append(df[actual_col] == val)
+            choices.append(f"{col_name}_{suffix}")
+
+        default_value = f"{col_name}_Z"
+        # Return vectorized evaluated result
+        return np.select(conditions, choices, default=default_value).tolist()
+
+    def get_audience_experiment_id(self, customer_cells: pd.DataFrame):
+
+        audience = self.experiment_settings.get("audience_experiments", {})
+        split_col = audience.get("split_col", "Audience")
+        audience_sample = audience.get("sample", ["Best"])
+        audience_split = audience.get("split")
+        audience_name = audience.get("name", "Audience")
+
+        audience_value = False
+        id = ""
+        if split_col in customer_cells.columns:
+            audience_value = customer_cells[split_col].values[0] | False
+
+        if (
+            not audience_value
+            or customer_cells["Fallow_Control"] == self.control_value
+        ):
+            id = f"Aud_{audience_name}_Z"
+        elif (
+            customer_cells["ShoppingBagTest1"].values[0] not in audience_sample
+        ):
+            id = f"Aud_{audience_name}_Z"
+        elif customer_cells[split_col] == audience_value:
+            id = f"Aud_{audience_name}_{audience_split}"
+        else:
+            id = f"Aud_{audience_name}_Z"
+
+        return id
+
+    def build_experiment_id(self, experiment_df: pd.DataFrame):
+
+        ids = []
+        if self.experiment_settings.get("audience_settings", {}).get(
+            "enabled", False
+        ):
+            audience_id = self.get_audience_experiment_id(experiment_df)
+            ids.extend(audience_id)
+        # Get the list of experiments
+        experiments = self.experiment_settings.get("experiments", {})
+
+        for key, value in experiments.items():
+            ids.extend(self.get_experiment_id(experiment_df, key, value))
+
+        experiment_id = " | ".join(ids)
+
+        return experiment_id
+
+    def build_payload_structure(
+        self, customer_cells_df: pd.DataFrame, results_df: pd.DataFrame
+    ) -> dict:
+
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            executor.submit(self.determine_control, customer_cells_df)
+            triggers = executor.submit(self.build_triggers, results_df)
+            experiment_id = executor.submit(
+                self.build_experiment_id, customer_cells_df
+            )
+            fragments = executor.submit(self.build_fragments, results_df)
+
+        payload = {
+            "ads": {
+                "adFatigueImpressionThreshold": self.ad_fatigue_threshold,
+                "experimentId": experiment_id.result(),
+                "triggers": triggers.result(),
+                "control": self.control,
+                "fragments": fragments.result(),
+            }
+        }
+
+        if self.ad_fatigue_active_locations:
+            hash_data = {
+                "account_number": customer_cells_df["AccountNumber"].values[0]
+            }
+            hash_data = hash_data | payload.get("ads", {})
+            payload["ads"]["adsHash"] = self.build_adshash(hash_data)
+
+        return payload
 
     def write_to_bloomreach(self):
+        # Multiprocess the steps here!!!
         # TODO Add in additional step here to write out to bloommreach API
         pass
+
+    # Functionality to build out payload structure
