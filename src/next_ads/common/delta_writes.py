862fb5fa3f556bacbc9219f79d930191aa28702d
Claire Wilson-Barnes <claire_wilsonbarnes@next.co.uk>
2026-08-19 20:11:33 +0100

    fix of lambda call

--- src/next_ads/common/delta_writes.py
+++ src/next_ads/common/delta_writes.py
@@ -596,9 +596,9 @@ def atomic_replace_where_by_name(
             build_delete_statement(
                 target_table=target_table, delete_scope=delete_scope
             )
-        )
+        
         if delete_scope
-        else lambda source_view, selected_columns: (
+        else 
             build_replace_where_statement(
                 target_table=target_table,
                 source_view=source_view,
